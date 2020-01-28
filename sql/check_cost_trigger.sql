75aefaf114e8e9b2b9dd5a64ae59a9a6d0b24aea
waghei <mwaghei78@gmail.com>
2020-01-28 15:58:56 +0330

    .

--- sql/check_cost_trigger.sql
+++ sql/check_cost_trigger.sql
@@ -1,4 +1,5 @@
 CREATE TRIGGER check_cost()
   after insert
   ON public."Cloud"
+  FOR EACH ROW 
   EXECUTE PROCEDURE delete_cloud();
--- sql/insert_transaction_function.sql
+++ sql/insert_transaction_function.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION insert_transaction()
+CREATE OR REPLACE FUNCTION insert_transaction
   RETURNS trigger AS
 $BODY$
 BEGIN
     INSERT public."Transaction"(amount,wallet_id)
-    Values(cost_per_day, select id from Wallet where user_id = new.user_id)
+    Values(cost_per_day, select id from Wallet where user_id = new.user_id);
 END;
 $BODY$ LANGUAGE plpgsql;  
